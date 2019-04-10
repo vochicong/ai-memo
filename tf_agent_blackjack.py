b738dc3987482db7f2c0362ecf53936f7e14e0f9
Cong <kongvc@gmail.com>
2019-04-10 20:24:08 +0900

    Deep Q-learning Blackjack

--- tf_agent_blackjack.py
+++ tf_agent_blackjack.py
@@ -14,7 +14,12 @@
 
 
 # %%
+# !which python
+# !sudo apt install -y cuda-cublas-10-0  cuda-cusolver-10-0 cuda-cudart-10-0 cuda-cusparse-10-0
+# !conda install -y -c anaconda cudatoolkit
+# !pip install tf-nightly-gpu tf-agents-nightly 'gym==0.10.11'
 from __future__ import absolute_import, division, print_function
+import matplotlib.pyplot as plt
 from tf_agents.drivers import dynamic_step_driver
 from tf_agents.replay_buffers import tf_uniform_replay_buffer
 from tf_agents.agents.dqn import q_network
@@ -22,10 +27,6 @@ from tf_agents.agents.dqn import dqn_agent
 from tf_agents.drivers import dynamic_episode_driver
 from tf_agents.metrics import tf_metrics
 from tf_agents.policies import random_tf_policy
-# !which python
-# !sudo apt install -y cuda-cublas-10-0  cuda-cusolver-10-0 cuda-cudart-10-0 cuda-cusparse-10-0
-# !conda install -y -c anaconda cudatoolkit
-# !pip install tf-nightly-gpu tf-agents-nightly 'gym==0.10.11'
 from tf_agents.environments import utils
 import numpy as np
 import tensorflow as tf
@@ -37,6 +38,16 @@ from tf_agents.environments import time_step
 from tf_agents.specs import array_spec
 
 tf.compat.v1.enable_v2_behavior()
+assert tf.executing_eagerly()
+# tf.enable_eager_execution()
+
+DEBUG = False
+num_eval_episodes = 5  # @param
+
+
+def plog(msg, *args):
+    if DEBUG:
+        print(msg.format(*args))
 
 
 class BlackJackEnv(py_environment.PyEnvironment):
@@ -44,25 +55,39 @@ class BlackJackEnv(py_environment.PyEnvironment):
     ACT_HIT = 0
     ACT_STICK = 1
     LIMIT_SCORE = 21
-    STATE_LEN = 3
 
-    def __init__(self):
+    def __init__(self, state_len=1):
+        self._batch_size = 1  # batch_size
+        self._state_len = state_len
         self._action_spec = array_spec.BoundedArraySpec(
             shape=(), dtype=np.int32, name='action',
             minimum=self.ACT_HIT, maximum=self.ACT_STICK,
         )
         self._observation_spec = array_spec.BoundedArraySpec(
-            shape=(self.STATE_LEN,), dtype=np.int32, minimum=0,
+            shape=(self._state_len,), dtype=np.int32, minimum=0,
             name='observation'
         )
         self.reset()
         return
 
     def _state(self):
+        if self._state_len == 1:
+            return self._state_player_sum()
+        return self._state_last_cards()
+
+    def _state_player_sum(self):
+        # Return the player current score
+        state = [np.sum(self._player_cards)]
+        return np.array(state, dtype=np.int32)
+
+    def _state_last_cards(self):
         # Full state includes 1st card of the dealer and all cards of player,
-        # but this return only the last STATE_LEN cards.
+        # but this return only the last _state_len cards.
         state = [self._dealer_cards[0]] + self._player_cards
-        return np.array(state[-self.STATE_LEN:], dtype=np.int32)
+        if len(state) < self._state_len:
+            state = np.pad(state, (0, self._state_len-len(state)),
+                           'constant', constant_values=(0))
+        return np.array(state[-self._state_len:], dtype=np.int32)
 
     def action_spec(self):
         return self._action_spec
@@ -95,10 +120,11 @@ class BlackJackEnv(py_environment.PyEnvironment):
         return np.sum(self._player_cards)
 
     def _terminate(self, reward):
-        print("Player: {} -> {}. Dealer: {} -> {}. Reward: {}.".format(
+        plog(
+            "Player: {} -> {}. Dealer: {} -> {}. Reward: {}.",
             self._player_cards, np.sum(self._player_cards),
             self._dealer_cards, np.sum(self._dealer_cards),
-            reward))
+            reward)
         self._episode_ended = True
         return time_step.termination(self._state(), reward)
 
@@ -124,6 +150,10 @@ class BlackJackEnv(py_environment.PyEnvironment):
             reward = -1
         return self._terminate(reward)
 
+    @classmethod
+    def tf_env(cls):
+        return tf_py_environment.TFPyEnvironment(cls())
+
 
 def print_spec(env):
     act_spec, ts_spec = env.action_spec(), env.time_step_spec()
@@ -132,8 +162,9 @@ def print_spec(env):
         print(x)
     return
 
+
 # TODO: validate_py_environment should check for a reset()
-# utils.validate_py_environment(BlackJackEnv())
+utils.validate_py_environment(BlackJackEnv())
 
 # %% [markdown]
 # ## for loop でランダムに遊ぶ場合
@@ -162,9 +193,8 @@ def play_blackjack(env, n_max_cards=1):
     return cards, gain
 
 
-env = tf_py_environment.TFPyEnvironment(BlackJackEnv())
+env = BlackJackEnv.tf_env()
 gains = []
-num_eval_episodes = 5  # @param
 for _ in range(num_eval_episodes):
     _, gain = play_blackjack(env, 2)
     gains.append(gain)
@@ -173,57 +203,51 @@ mean_score1
 
 # %% [markdown]
 # ## RandomTFPolicyでランダムに遊ぶ
+#
+# Agentに見せる環境の情報 (state) は2パターン
+# - プレイヤーのカードの合計値
+# - ディーラーの最初のカードと、プレイヤーが引いた全てのカード
 # %%
-num_eval_episodes = 5  # @param
 
 
 def evaluate_policy(
         policy,
-        observers=None,
-        eval_env=tf_py_environment.TFPyEnvironment(BlackJackEnv()),
-        num_episodes=num_eval_episodes):
+        num_episodes=num_eval_episodes,
+        eval_env=BlackJackEnv.tf_env(),
+):
+    avg_return = tf_metrics.AverageReturnMetric()
+    # n_episodes = tf_metrics.NumberOfEpisodes()
+    # n_steps = tf_metrics.EnvironmentSteps()
+    observers = [avg_return,
+                 #  n_episodes, n_steps
+                 ]
     driver = dynamic_episode_driver.DynamicEpisodeDriver(
         eval_env, policy, observers, num_episodes)
     final_step, policy_state = driver.run(num_episodes=num_episodes)
-    print('Final step', final_step)
-    return driver, final_step, policy_state
+    # print('Number of Steps: ', n_steps.result().numpy())
+    # print('Number of Episodes: ', n_episodes.result().numpy())
+    # print('Average Return: ', avg_return.result().numpy())
+    return driver, final_step, policy_state, avg_return.result().numpy()
 
 
-env = tf_py_environment.TFPyEnvironment(BlackJackEnv())
+DEBUG = False
+env = BlackJackEnv.tf_env()
 rand_policy = random_tf_policy.RandomTFPolicy(
     action_spec=env.action_spec(),
     time_step_spec=env.time_step_spec(),)
-replay_buffer = []
-avg_return = tf_metrics.AverageReturnMetric()
-n_episodes = tf_metrics.NumberOfEpisodes()
-n_steps = tf_metrics.EnvironmentSteps()
-observers = [replay_buffer.append, avg_return, n_episodes, n_steps]
-driver, final_step, policy_state = evaluate_policy(rand_policy, observers)
-print('Number of Steps: ', n_steps.result().numpy())
-print('Number of Episodes: ', n_episodes.result().numpy())
-print('Average Return: ', avg_return.result().numpy())
-
 # %%
-final_step, policy_state = driver.run(final_step, policy_state, num_episodes=1)
-print('Number of Steps: ', n_steps.result().numpy())
-print('Number of Episodes: ', n_episodes.result().numpy())
-print('Average Return: ', avg_return.result().numpy())
+avg_returns = []
+for n_episodes in range(100, 210, 10):
+    _, _, _, avg_return = evaluate_policy(rand_policy, num_episodes=n_episodes)
+    avg_returns.append(avg_return)
+
+
+plt.plot(avg_returns)
 
 # %% [markdown]
 # ## DQNで強化学習
 
 # %%
-
-
-learning_rate = 1e-3  # @param
-replay_buffer_capacity = 1000  # @param
-fc_layer_params = (100, 100)  # @param
-num_iterations = 5000  # @param
-initial_collect_steps = 1000  # @param
-batch_size = 64  # @param
-collect_steps_per_iteration = 10  # @param
-log_interval = 200  # @param
-eval_interval = 1000  # @param
 
 
 class DqnAgent:
@@ -255,16 +279,16 @@ class DqnAgent:
         # Replay Bufferの初期化。初期データ収集
         self.replay_buffer = buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
             data_spec=self.agent.collect_data_spec,
-            # batch_size=self.env.batch_size,
-            batch_size=batch_size,
+            batch_size=self.env.batch_size,  # actually 1, env isn't batched
             max_length=replay_buffer_capacity
         )
         print(buffer.capacity.numpy(), buffer._batch_size)
         print(buffer.data_spec)
         self._collect_data(rand_policy, initial_collect_steps)
         dataset = buffer.as_dataset(
-            num_parallel_calls=3, sample_batch_size=batch_size, num_steps=2
-        ).prefetch(3)
+            num_parallel_calls=3, num_steps=2,
+            sample_batch_size=batch_size,
+        ).prefetch(batch_size)
         self.data_iterator = iter(dataset)
 
     def _collect_data(self, policy, n_steps):
@@ -274,25 +298,52 @@ class DqnAgent:
         ).run()
         return
 
-    def train(self, num_iterations=num_iterations):
-        avg_return = evaluate_policy(self.agent.policy, num_eval_episodes)
+    def train(self, num_iterations):
+        _, _, _, avg_return = evaluate_policy(
+            self.agent.policy, num_eval_episodes)
         avg_returns = [avg_return]
         for step in range(1, 1 + num_iterations):
             self._collect_data(self.agent.collect_policy,
                                collect_steps_per_iteration)
             experience, _ = next(self.data_iterator)
             train_loss = self.agent.train(experience)
-            if step % log_interval == 0:
-                print('Step = {}, Loss= {}'.format(step, train_loss.loss))
-            if step % eval_interval == 0:
-                avg_return = evaluate_policy(
-                    self.agent.policy, num_eval_episodes)
-                print('Step = {}, Average Return= {}'.format(step, avg_return))
-                avg_returns.append(avg_return)
+            self._print_log(step, train_loss.loss, avg_returns)
         return avg_returns
 
-env=BlackJackEnv()
-dqn = DqnAgent(env)
+    def _print_log(self, step, loss, avg_returns):
+        if step % log_interval == 0:
+            print(f'Step {step: >3}. Loss {loss}.')
+        if step % eval_interval == 0:
+            _, _, _, avg_return = evaluate_policy(
+                self.agent.policy, num_eval_episodes)
+            print(f'Step {step: >3}. AvgReturn {avg_return}.')
+            avg_returns.append(avg_return)
+
+
+def plot(avg_returns, num_iterations, eval_interval):
+    steps = range(0, num_iterations + 1, eval_interval)
+    plt.ylabel('Average Return')
+    plt.xlabel('Step')
+    plt.plot(steps, avg_returns)
+    # plt.ylim(top=210)
+
+
+DEBUG = False
+learning_rate = 1e-3  # @param
+batch_size = 64*4  # @param
+collect_steps_per_iteration = 100  # @param
+initial_collect_steps = collect_steps_per_iteration*1  # @param
+num_eval_episodes = collect_steps_per_iteration  # @param
+replay_buffer_capacity = collect_steps_per_iteration*10  # @param
+fc_layer_params = (100, 100, )  # @param
+log_interval = 10  # @param
+eval_interval = log_interval*5  # @param
+num_iterations = eval_interval*3  # @param
+assert eval_interval % log_interval == 0
+
+# Set a `bad` _state_len and see that it can't learn
+dqn = DqnAgent(BlackJackEnv.tf_env())
+avg_returns = dqn.train(num_iterations)
+plot(avg_returns, num_iterations, eval_interval)
 
 # %%
-# 以上
